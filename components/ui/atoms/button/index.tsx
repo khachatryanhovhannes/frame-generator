1679f1f0f7fe965bf0a795ea4da2e98db2211a71
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-04-27 15:52:19 +0400

    Change main button cursor to pointer

--- components/ui/atoms/button/index.tsx
+++ components/ui/atoms/button/index.tsx
@@ -10,7 +10,7 @@ function Button({ text, link }: IButtonProps) {
   return (
     <Link
       href={link}
-      className="mt-6 inline-block bg-green-700 hover:bg-green-800 text-white font-semibold px-8 py-3 rounded-lg shadow-md transition"
+      className="mt-6  cursor-pointer inline-block bg-green-700 hover:bg-green-800 text-white font-semibold px-8 py-3 rounded-lg shadow-md transition"
     >
       {text}
     </Link>
